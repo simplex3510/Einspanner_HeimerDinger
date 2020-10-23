400a580e5f51ca94e700eac581f04e28a7a5aa66
simplex3510 <leeyj5>
2020-10-24 01:14:55 +0900

    [20.10.23(금)] 메인, 테트리스 보드 GUI 분석 후 주석 추가

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] arg) {
-        System.out.println("Second Test Commit");
+        Scanner sc = new Scanner(System.in);
+        int line = sc.nextInt();
+        int num = 1;
+
+        for (int i=0; i<line; i++) {
+            for (int j=0; j<line; j++){
+                System.out.print(num + " ");
+                num += 2;
+
+                if (10 <= num)
+                    num = 1;
+            }
+            System.out.print('\n');
+        }
+
     }
 }
+
+
